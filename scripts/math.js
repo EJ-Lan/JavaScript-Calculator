09ed322d115e33c0e6edbdd2b50a28c214f90778
EJ-Lan <ervinjon_lanada@outlook.com>
2023-12-20 22:49:23 -0700

    Add html for calculator

--- scripts/math.js
+++ scripts/math.js
@@ -1,21 +1,19 @@
-let add = (a, b) => {
+export let add = (a, b) => {
     return a + b;
 }
 
-let subtract = (a, b) => {
+export let subtract = (a, b) => {
     return a - b;
 }
 
-let multiply = (a, b) => {
+export let multiply = (a, b) => {
     return a * b;
 }
 
-let divide = (a, b) => {
+export let divide = (a, b) => {
     if (b != 0) {
         return a / b;
     } else {
         return NaN // If NaN then cannot divide
     }
 }
-
-module.exports = { add, subtract, multiply, divide };
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 import { add, subtract, multiply, divide } from './math.js';
+
+let firstNum = 0;
+let secondNum = 0;
+let operator;
+
+let operate = (operator, firstNum, secondNum) => {
+    return operator(firstNum, secondNum);
+}
+
+// Make operate globally accessible for testing
+window.operate = operate;
+window.add = add;
+window.subtract = subtract;
+window.multiply = multiply;
+window.divide = divide;
